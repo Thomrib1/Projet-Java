57078fa7c154edc72948145f2ef784c21e4cd524
Thomas <thomas.ribeiro@ynov.com>
2025-01-27 12:41:59 +0100

    essai d'un programme avec swing pour afficher une fenetre vierge

--- Projet-Java/Swing/InterfaceGraphique.java
+++ Projet-Java/Swing/InterfaceGraphique.java
@@ -1,3 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
 public class InterfaceGraphique {
-    
+
+  public static void main(String[] args) {
+    JFrame window = new JFrame("Première appli");
+    window.add(new JLabel("test  swing vierge"));
+    window.setSize(300,100);
+    window.setLocation(100, 100);
+    window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    window.setVisible(true);
+  }
 }
