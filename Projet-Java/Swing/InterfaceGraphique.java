128a73210222842a0eb5ca6430cb969eb4dde685
Thomas <thomas.ribeiro@ynov.com>
2025-01-27 12:43:51 +0100

    change title

--- Projet-Java/Swing/InterfaceGraphique.java
+++ Projet-Java/Swing/InterfaceGraphique.java
@@ -5,7 +5,7 @@ import javax.swing.WindowConstants;
 public class InterfaceGraphique {
 
   public static void main(String[] args) {
-    JFrame window = new JFrame("Première appli");
+    JFrame window = new JFrame("Jeu de dames");
     window.add(new JLabel("test  swing vierge"));
     window.setSize(300,100);
     window.setLocation(100, 100);
