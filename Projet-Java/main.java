dfcc3386a9e290d02e050940833cb8f52c52b807
Thomas <thomas.ribeiro@ynov.com>
2025-01-27 10:24:51 +0100

    add joueurcourant + graphic interface

--- Projet-Java/main.java
+++ Projet-Java/main.java
@@ -1,5 +1,7 @@
 public class main {
-    private Plateau plateau;
+    private Plateau plateau; // on crée 4 variables privées (ne peut être accèder que de puis le main ) servant à stocker les objets Plateau et Joueurs 
     private Joueur joueur1;
     private Joueur joueur2;
+    private Joueur joueurCourant; // permet de suivre quel joueur doit jouer à tel moment dans la partie
+    private InterfaceGraphique interfaceGraphique;
 }
