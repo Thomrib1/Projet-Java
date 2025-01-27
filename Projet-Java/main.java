3efe00295199439ce36113aa499fc93e9b413d28
Thomas <thomas.ribeiro@ynov.com>
2025-01-27 10:51:44 +0100

    instances java

--- Projet-Java/main.java
+++ Projet-Java/main.java
@@ -4,4 +4,12 @@ public class main {
     private Joueur joueur2;
     private Joueur joueurCourant; // permet de suivre quel joueur doit jouer à tel moment dans la partie
     private InterfaceGraphique interfaceGraphique;
+
+    public Jeu() {
+        this.plateau = new Plateau(); // on crée un plateau
+        this.joueur1 = new Joueur("Joueur 1", true);  // joueur 1 utilisera les pièces blanches
+        this.joueur2 = new Joueur("Joueur 2", false); // joueur 2 utilisera les pièces noires
+        this.joueurCourant = joueur1;
+        this.gui = new InterfaceGraphique(this); // lie l'interface graphique à la partie 
+    }
 }
