b903ec3a3b56a2cccb1e03aacdfe6da5bc506224
Thomas <thomas.ribeiro@ynov.com>
2025-01-28 18:13:31 +0100

    height defined

--- Projet-Java/Plateau.java
+++ Projet-Java/Plateau.java
@@ -1,3 +1,8 @@
 public class Plateau {
-    
-}
+    private Case[][] cases;
+    private static final int TAILLE = 10;
+
+    public Plateau() {
+        cases = new Case[TAILLE][TAILLE];
+        initialiserPlateau();
+    }
