0870a5d8bcdc637f3695cb5dc586c3b1c136faf1
Thomas <thomas.ribeiro@ynov.com>
2025-01-29 17:52:58 +0100

    build full class Piece

--- Projet-Java/Piece.java
+++ Projet-Java/Piece.java
@@ -1,3 +1,11 @@
-public class Piece {
-    
+public abstract class Piece {
+    protected boolean estBlanc; // Cette variable indique si la pièce est blanche (true) ou noire (false).
+
+    public Piece(boolean estBlanc) {
+        this.estBlanc = estBlanc;// initialise la couleur de la pièce.
+    }
+
+    public boolean estBlanc() {
+        return estBlanc; // Retourne la couleur de la pièce.
+    }
 }
