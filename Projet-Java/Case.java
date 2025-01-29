ffd2885917765b02e567e8a73bfc74e018f6d5d9
Thomas <thomas.ribeiro@ynov.com>
2025-01-29 17:21:30 +0100

    add 3 variables instances + coordonées de lignes/colonnes

--- Projet-Java/Case.java
+++ Projet-Java/Case.java
@@ -1,3 +1,10 @@
-public class Case {
-    
-}
+public class Case { // Classe qui représente une case du plateau de jeu
+    private int ligne;
+    private int colonne;
+    private Piece piece;
+
+    public Case(int ligne, int colonne) {// Constructeur de la classe Case
+        this.ligne = ligne;
+        this.colonne = colonne;
+        this.piece = null;
+    }
