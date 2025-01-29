257a55ed73a94d297522e620d71c63db5584ca10
Thomrib1 <thomas.ribeiro@ynov.com>
2025-01-29 22:17:35 +0100

    Delete Projet-Java directory
    problême de dossiers en double

--- Projet-Java/JeuDeLaDame.java
+++
@@ -1,7 +0,0 @@
-public class JeuDeLaDame {
-    public static void main(String[] args) {
-        Jeu jeu = new Jeu();
-        jeu.jouer();
-    }
-}
-// Avec main on peut lancer notre jeu de dame en executant la classe JeuDeLaDame.java ! 
