f93d96a1e97c80b7caae32cd8acb3dbf89b97c6a
Thomas <thomas.ribeiro@ynov.com>
2025-01-29 17:56:33 +0100

    build full class dame

--- Projet-Java/Dame.java
+++ Projet-Java/Dame.java
@@ -1,3 +1,10 @@
-public class Dame {
-    
+public class Dame extends Piece {
+    public Dame(boolean estBlanc) {
+        super(estBlanc);// Appel du constructeur de la classe mère
+    }
+
+    @Override
+    public String toString() {
+        return estBlanc ? "OD" : "XD";// Return la string représentant la pièce
+    }
 }
